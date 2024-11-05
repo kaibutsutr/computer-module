a87bcf6cae192925862cb276215ab54923d1e81d
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2024-11-06 00:40:55 +0300

    1212

--- src/computer/computer.module.ts
+++ src/computer/computer.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ComputerController } from './computer.controller';
 import { PowerModule } from 'src/power/power.module';
+import { DiskModule } from 'src/disk/disk.module';
+import { CpuModule } from 'src/cpu/cpu.module';
 
 @Module({
   controllers: [ComputerController],
-  imports: [PowerModule],
+  imports: [PowerModule, DiskModule, CpuModule],
 })
 export class ComputerModule {}
