f33132c74aea4cb4382d6717d110ebb09e4a4b8f
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2024-11-06 00:28:40 +0300

    123

--- src/disk/disk.module.ts
+++ src/disk/disk.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DiskService } from './disk.service';
+import { PowerService } from 'src/power/power.service';
 
 @Module({
-  providers: [DiskService]
+  providers: [DiskService],
+  imports: [PowerService],
 })
 export class DiskModule {}
--- src/disk/disk.service.ts
+++ src/disk/disk.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
 
 @Injectable()
-export class DiskService {}
+export class DiskService {
+  constructor(private powerService: PowerService) {} // we use power service and its methods in our diskservice so we need them
+  getData() {
+    console.log('Drawing 3 watts from Power Supply');
+    this.powerService.supplyPower(3);
+    return 'data';
+  }
+}
