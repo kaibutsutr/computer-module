9a9d007f31eb813ef0691d630f8453742e941742
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2024-11-06 00:23:50 +0300

    disk

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service'; // import power service since cpu is dependant on it
 
 @Injectable()
-export class CpuService {}
+export class CpuService {
+  constructor(private powerService: PowerService) {
+    // everytime a cpu service is created, it uses the power service
+  }
+  compute(a: number, b: number) {
+    console.log('Drawing 1 watt from Power supply');
+    this.powerService.supplyPower(1); // we use this operator here because we are using this instance of the object
+
+    return a * b;
+  }
+}
--- src/power/power.module.ts
+++ src/power/power.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PowerService } from './power.service';
 
 @Module({
-  providers: [PowerService]
+  providers: [PowerService], // dependency auto created for DI
+  exports: [PowerService], // we can export this service to other modules with this code
 })
 export class PowerModule {}
