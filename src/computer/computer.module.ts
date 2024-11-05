1654f6f08500d021ed18a6c9707613e4e4cb5560
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2024-11-06 00:34:32 +0300

    1221

--- src/computer/computer.module.ts
+++ src/computer/computer.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ComputerController } from './computer.controller';
+import { PowerModule } from 'src/power/power.module';
 
 @Module({
-  controllers: [ComputerController]
+  controllers: [ComputerController],
+  imports: [PowerModule],
 })
 export class ComputerModule {}
--- src/cpu/cpu.module.ts
+++ src/cpu/cpu.module.ts
@@ -5,5 +5,6 @@ import { PowerModule } from 'src/power/power.module';
 @Module({
   providers: [CpuService],
   imports: [PowerModule], // import powermodule here so our services are all exported
+  exports: [CpuService], // these are private by default so we need exports to carry them between modules
 })
 export class CpuModule {}
